59cd75afc654dcd5c661c7797a3bab5b4fc72a76
namatigi <leon.mangu@gmail.com>
2017-06-26 18:24:11 +0300

    Files added

--- database.php
+++ database.php
@@ -1,30 +1,43 @@
 <?php
 	#create database connection.
-	// $link = new mysqli("localhost","root","Paulina@959");
+	$link = mysqli_connect('localhost','root','Paulina@1959');
 
-	// if($link->connect_errno){
-	// 	echo "Failed to connect to mysql: (".$link->connect_errno.")".$link->connect_error;		
-	// }else{
-
-	// echo "Successfully connected";
-	// }
-
-	$link = mysql_connect('localhost','root','Paulina@1959');
-
-	if($link){
-		echo "Connected";
-	}else{
-		echo "Disconnected";
-	}
-
+	if(!$link){
+	    die("Database connection failed: ".mysqli_connect_error());
+    }else{
+	    #select a database to use
+	    $db_select = mysqli_select_db($link,'widget_corp');
+	    if(!$db_select){
+	        die("Database selection failed:  ".mysqli_connect_error());
+        }
+    }
 ?>
 
 <html>
 	<head>
-		<title>Include</title>
+		<title>Database</title>
 	</head>
 	<body>
-	
+    <?php
+    #perform database query.
+    $result = mysqli_query($link,"select * from subject");
+
+    if(!($result)){
+        die("No results found: ".mysqli_connect_error());
+    }
+
+    #Use returned data.
+    while($row=mysqli_fetch_array($result)){
+        #echo $row[1]. " ".$row[2]."<br/>";
+        echo $row['menu_name']. " ". $row['position']."<br/>";
+    }
+    // var_dump($result);
+?>
 
 	</body>
-</html> 
+</html>
+
+<?php
+
+mysqli_close($link);
+?>
---
+++ encryption.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xnet1872
+* Date: 26/06/2017
+* Time: 18:21
+*/
+
+
+$str = "Paulina@1959";
+
+echo md5($str);
+
+?>
